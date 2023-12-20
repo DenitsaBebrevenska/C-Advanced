c132f2ac2faa966c29c00a48ed505e5a8d035383
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-12-20 23:37:46 +0200

    KeyRevolver added but 77/100

--- StacksAndQueues/StacksAndQueues/KeyRevolver/Program.cs
+++ StacksAndQueues/StacksAndQueues/KeyRevolver/Program.cs
@@ -1,69 +1,55 @@
-﻿using System.Collections;
-
-namespace KeyRevolver
+﻿namespace KeyRevolver
 {
-	internal class Program
-	{
-		static void Main()
-		{
-			//•	On the first line of input, you will receive the price of each bullet – an integer in the range [0…100].
-			// •	On the second line, you will receive the size of the gun barrel – an integer in the range [1…5000].
-			// •	On the third line, you will receive the bullets – a space-separated integer sequence with [1…100] integers.
-			// •	On the fourth line, you will receive the locks – a space-separated integer sequence with [1…100] integers.
-			// •	On the fifth line, you will receive the value of the intelligence – an integer in the range [1…100000].
+    internal class Program
+    {
+        static void Main()
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+            Stack<int> bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Queue<int> locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            int valueOfIntelligence = int.Parse(Console.ReadLine());
+            int totalBullets = bullets.Count;
 
-			int bulletPrice = int.Parse(Console.ReadLine());
-			int barrelSize = int.Parse(Console.ReadLine());
-			Stack<int> bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
-			Queue<int> locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
-			int valueOfIntelligence = int.Parse(Console.ReadLine());
-			Queue<Barrel> barrels = new Queue<Barrel>();
+            while (locks.Count > 0)
+            {
+                int currentLock = locks.Peek();
 
-			for (int i = 0; i < barrelSize; i++)
-			{
-				barrels.Enqueue(new Barrel(bullets));
-			}
+                while (bullets.Count > 0)
+                {
+                    bool targetShot = false;
+                    int currentBullet = bullets.Pop();
 
-			int currentLock = locks.Dequeue();
-			Barrel currentBarrel = barrels.Dequeue();
+                    if (currentBullet <= currentLock)
+                    {
+                        Console.WriteLine("Bang!");
+                        locks.Dequeue();
+                        targetShot = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ping!");
+                    }
 
-			while (locks.Count > 0 && barrels.Count > 0)
-			{
-				if (bullets.Pop() <= currentLock)
-				{
-					Console.WriteLine("Bang!");
-					currentLock = locks.Dequeue();
-				}
-				else
-				{
-					Console.WriteLine("Ping!");
-				}
+                    if (bullets.Count % barrelSize == 0 && bullets.Count > 0)
+                    {
+                        Console.WriteLine("Reloading!");
+                    }
 
-				totalBulletCount --;
-				bulletsPerBarrel--;
+                    if (targetShot)
+                    {
+                        break;
+                    }
+                }
 
-				if (bulletsPerBarrel == 0)
-				{
-					if (totalBulletCount == 0)
-					{
-						continue;
-					}
-
-					Console.WriteLine("Reloading!");
-					bulletsPerBarrel = 
-				}
-			}
-
-		}
-
-		internal class Barrel
-		{
-			public Stack<int> Bullets { get; set; }
-
-			public Barrel(Stack<int> bullets)
-			{
-				Bullets = new Stack<int>();
-			}
-		}
-	}
+                if (bullets.Count == 0 && locks.Count > 0)
+                {
+                    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+                    return;
+                }
+                
+            }
+            Console.WriteLine($"{bullets.Count} bullets left. Earned ${valueOfIntelligence - (totalBullets - bullets.Count) * bulletPrice}");
+        }
+    }
 }
