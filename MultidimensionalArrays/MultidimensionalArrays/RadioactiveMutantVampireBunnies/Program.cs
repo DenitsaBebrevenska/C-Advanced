396cdb85a7ea6c230914adf515388ca6d57a5096
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-12-28 23:14:15 +0200

    RadioactiveMutantVampireBunnies added

--- MultidimensionalArrays/MultidimensionalArrays/RadioactiveMutantVampireBunnies/Program.cs
+++ MultidimensionalArrays/MultidimensionalArrays/RadioactiveMutantVampireBunnies/Program.cs
@@ -47,27 +47,47 @@
 
                 if (ExitsTheLair(playerRowLocation, playerColLocation, bunnyLair))
                 {
-                    Console.WriteLine($"won: {lastPosition[0]} {lastPosition[1]}");
                     bunnyLair[lastPosition[0], lastPosition[1]] = '.';
+                    MultiplyBunnies(bunnyLair);
+                    PrintLairFinalState(bunnyLair);
+                    Console.WriteLine($"won: {lastPosition[0]} {lastPosition[1]}");
+                    break;
                 }
 
                 if (KilledByBunny(playerRowLocation, playerColLocation, bunnyLair))
                 {
-                    Console.WriteLine($"dead: {playerRowLocation} {playerColLocation}");
                     bunnyLair[lastPosition[0], lastPosition[1]] = '.';
+                    MultiplyBunnies(bunnyLair);
+                    PrintLairFinalState(bunnyLair);
+                    Console.WriteLine($"dead: {playerRowLocation} {playerColLocation}");
+                    break;
                 }
 
-                if (!ExitsTheLair(playerRowLocation, playerColLocation, bunnyLair) 
-                    && !KilledByBunny(playerRowLocation, playerColLocation, bunnyLair))
-                {
-                    bunnyLair[lastPosition[0], lastPosition[1]] = '.';
-                    lastPosition[0] = playerRowLocation;
-                    lastPosition[1] = playerColLocation;
-                }
-                
+                bunnyLair[lastPosition[0], lastPosition[1]] = '.';
+                lastPosition[0] = playerRowLocation;
+                lastPosition[1] = playerColLocation;
+
                 MultiplyBunnies(bunnyLair);
 
+                if (KilledByBunny(lastPosition[0], lastPosition[1], bunnyLair))
+                {
+                    PrintLairFinalState(bunnyLair);
+                    Console.WriteLine($"dead: {lastPosition[0]} {lastPosition[1]}");
+                    break;
+                }
+            }
+        }
 
+        static void PrintLairFinalState(char[,] bunnyLair)
+        {
+            for (int i = 0; i < bunnyLair.GetLength(0); i++)
+            {
+                for (int j = 0; j < bunnyLair.GetLength(1); j++)
+                {
+                    Console.Write(bunnyLair[i, j]);
+                }
+
+                Console.WriteLine();
             }
         }
 
@@ -82,7 +102,42 @@
 
         static void MultiplyBunnies(char[,] bunnyLair)
         {
-            
+            List<int[]> coordinateNewbornBunnies = new List<int[]>();
+
+            for (int i = 0; i < bunnyLair.GetLength(0); i++)
+            {
+                for (int j = 0; j < bunnyLair.GetLength(1); j++)
+                {
+                    if (bunnyLair[i, j] != 'B')
+                    {
+                        continue;
+                    }
+
+                    List<int[]> coordinatesMultiplication = new List<int[]>()
+                    {
+                        new int[]{i - 1, j},
+                        new int[]{i + 1, j},
+                        new int[]{i, j - 1},
+                        new int[]{i, j + 1}
+                    };
+
+                    for (int k = 0; k < coordinatesMultiplication.Count; k++)
+                    {
+                        if (!AreValidCoordinates(coordinatesMultiplication[k], bunnyLair)
+                            || bunnyLair[coordinatesMultiplication[k][0], coordinatesMultiplication[k][1]] == 'B')
+                        {
+                            continue;
+                        }
+
+                        coordinateNewbornBunnies.Add(coordinatesMultiplication[k]);
+                    }
+                }
+            }
+
+            for (int i = 0; i < coordinateNewbornBunnies.Count; i++)
+            {
+                bunnyLair[coordinateNewbornBunnies[i][0], coordinateNewbornBunnies[i][1]] = 'B';
+            }
         }
 
         static bool KilledByBunny(int playerRowLocation, int playerColLocation, char[,] bunnyLair)
@@ -96,9 +151,13 @@
                    playerColLocation < 0 || playerColLocation >= bunnyLair.GetLength(1);
         }
 
-        static bool AreValidCoordinates()
+        static bool AreValidCoordinates(int[] coordinates, char[,] bunnyLair)
         {
+            int row = coordinates[0];
+            int col = coordinates[1];
 
+            return row >= 0 && row < bunnyLair.GetLength(0)
+                && col >= 0 && col < bunnyLair.GetLength(1);
         }
     }
 }
