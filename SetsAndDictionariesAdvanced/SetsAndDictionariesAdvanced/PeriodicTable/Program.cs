a2169b92ef096aca1fecc4fe3c44a87ebdf80e9f
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-12-29 23:41:39 +0200

    Periodic Table added

---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced/PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PeriodicTable
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            byte entryCount = byte.Parse(Console.ReadLine());
+            HashSet<string> elements = new HashSet<string>();
+
+            for (int i = 0; i < entryCount; i++)
+            {
+                string[] currentEntry = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < currentEntry.Length; j++)
+                {
+                    elements.Add(currentEntry[j]);
+                }
+            }
+
+            foreach (string element in elements.OrderBy(e => e))
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
