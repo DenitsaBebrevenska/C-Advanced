2cab1b8a5b9bdd60ca81801609f0ead2516694ee
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-12-29 23:36:52 +0200

    SetsOfElements

---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced/SetsOfElements/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SetsOfElements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+            int[] entries = Console.ReadLine().
+                Split().
+                Select(int.Parse).
+                ToArray();
+
+            for (int i = 0; i < entries[0] + entries[1]; i++)
+            {
+                if (i < entries[0])
+                {
+                    firstSet.Add(int.Parse(Console.ReadLine()));
+                    continue;
+                }
+
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach (int number in firstSet)
+            {
+                if (secondSet.Contains(number))
+                {
+                    Console.Write(number + " ");
+                }
+            }
+
+        }
+    }
+}
