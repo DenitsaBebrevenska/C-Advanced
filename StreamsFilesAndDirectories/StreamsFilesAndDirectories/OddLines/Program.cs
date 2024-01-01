21fe7ef287d5e95cc4ab5bfad0d8e772709d7cf7
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2024-01-01 23:09:53 +0200

    Folder Streams, Files and Directories created

---
+++ StreamsFilesAndDirectories/StreamsFilesAndDirectories/OddLines/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                //TODO
+            }
+        }
+    }
+}
