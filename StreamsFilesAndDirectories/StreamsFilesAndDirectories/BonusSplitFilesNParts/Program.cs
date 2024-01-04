93d6919affd1eb62563a886f0303f20b8c938bda
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2024-01-04 10:41:50 +0200

    added checkuserinput SplitFilesNParts

--- StreamsFilesAndDirectories/StreamsFilesAndDirectories/BonusSplitFilesNParts/Program.cs
+++ StreamsFilesAndDirectories/StreamsFilesAndDirectories/BonusSplitFilesNParts/Program.cs
@@ -2,9 +2,51 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            string directory = "../../../input.txt";
+            Console.WriteLine("How many parts do you want to split the input.txt file into?");
+            string input = Console.ReadLine();
+
+            byte[] fileBytes = File.ReadAllBytes(directory);
+
+            while (!CheckUserInput(input, fileBytes.Length))
+            {
+                input = Console.ReadLine();
+            }
+        }
+
+        static bool CheckUserInput(string input, long fileLength)
+        {
+            long partsCount;
+
+            try
+            {
+                partsCount = long.Parse(input);
+            }
+            catch (Exception parsingException)
+            {
+                Console.WriteLine("Please enter a number!");
+                return false;
+            }
+            if (partsCount < 0)
+            {
+                Console.WriteLine("The parts count cannot be a negative number! Enter a new count!");
+                return false;
+            }
+
+            if (partsCount > fileLength)
+            {
+                Console.WriteLine("Cannot split file into more parts than its length! Enter a new count!");
+                return false;
+            }
+
+            return true;
+        }
+
+        static void ParseInput()
+        {
+
         }
     }
 }
