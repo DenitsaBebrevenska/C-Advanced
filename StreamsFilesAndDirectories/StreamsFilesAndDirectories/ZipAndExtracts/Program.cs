ce990472fa2732cbb4a23ef99875b5079c0fcaa4
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2024-01-06 23:30:18 +0200

    Zip and Extracts added

--- StreamsFilesAndDirectories/StreamsFilesAndDirectories/ZipAndExtracts/Program.cs
+++ StreamsFilesAndDirectories/StreamsFilesAndDirectories/ZipAndExtracts/Program.cs
@@ -10,16 +10,25 @@
         {
             string inputFilePath = @"../../../copyMe.png";
             string zipArchiveFilePath = @"../../../archive.zip";
+            string fileName = "extracted.png";
+            string outputFilePath = @"../../..";
             ZipFileToArchive(inputFilePath, zipArchiveFilePath);
+            ExtractFileFromArchive(zipArchiveFilePath, fileName, outputFilePath);
         }
 
         public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
         {
-            ZipFile.CreateFromDirectory(inputFilePath, zipArchiveFilePath);
+            using ZipArchive zip = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create);
+            zip.CreateEntryFromFile(inputFilePath, "copyMe.png");
         }
 
         public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
         {
+           using ZipArchive zip = ZipFile.OpenRead(zipArchiveFilePath);
+           foreach (var entry in zip.Entries)
+           {
+               entry.ExtractToFile(Path.Combine(outputFilePath, fileName));
+           }
 
         }
     }
