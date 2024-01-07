7ec3afc66a0e4b7c5a8461702ccdb72b66ee06c9
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2024-01-08 00:10:50 +0200

    Knights of Honor added

---
+++ FunctionalProgramming/FunctionalProgramming/KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace KnightsOfHonor
+{
+    internal class Program
+    {
+        static Action<string> printSirName = name => Console.WriteLine($"Sir {name}");
+        static void Main()
+        {
+            //Create a program that reads a collection of names as strings from the console, appends "Sir" in front of every name and prints it back in the console. Use Action<T>.
+            List<string> names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            names.ForEach(printSirName);
+        }
+    }
+}
