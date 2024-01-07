75e22aeddbbc72c4a4a30025e7ec3650a1e100bd
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2024-01-08 00:24:31 +0200

    CustomMinFunction added

---
+++ FunctionalProgramming/FunctionalProgramming/CustomMinFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Create a simple program that reads from the console a set of integers and prints back on the console the smallest number from the collection. Use Func<T, T>.
+
+            int[] numbers = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Func<int, int, int> smallerNumber = (a, b) => a < b ? a : b;
+            Console.WriteLine(numbers.Aggregate(smallerNumber));
+        }
+    }
+}
--- FunctionalProgramming/FunctionalProgramming/KnightsOfHonor/Program.cs
+++ FunctionalProgramming/FunctionalProgramming/KnightsOfHonor/Program.cs
@@ -2,14 +2,13 @@
 {
     internal class Program
     {
-        static Action<string> printSirName = name => Console.WriteLine($"Sir {name}");
         static void Main()
         {
             //Create a program that reads a collection of names as strings from the console, appends "Sir" in front of every name and prints it back in the console. Use Action<T>.
             List<string> names = Console.ReadLine()
                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
-
+            Action<string> printSirName = name => Console.WriteLine($"Sir {name}");
             names.ForEach(printSirName);
         }
     }
