654ae046fc3359939363e7c4811553e21133d32d
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2024-01-08 22:43:27 +0200

    CustomMinFunction updated

--- FunctionalProgramming/FunctionalProgramming/CustomMinFunction/Program.cs
+++ FunctionalProgramming/FunctionalProgramming/CustomMinFunction/Program.cs
@@ -6,12 +6,31 @@
         {
             //Create a simple program that reads from the console a set of integers and prints back on the console the smallest number from the collection. Use Func<T, T>.
 
-            int[] numbers = Console.ReadLine()
-                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+            HashSet<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
-                .ToArray();
-            Func<int, int, int> smallerNumber = (a, b) => a < b ? a : b;
-            Console.WriteLine(numbers.Aggregate(smallerNumber));
+                .ToHashSet();
+            Func<int, int, int> minNumber = (a, b) => a < b ? a : b;
+            Console.WriteLine(numbers.Aggregate(minNumber));
+
+            //or can write the func to aggregate
+
+            //Func<HashSet<int>, int> minNumber = numbers =>
+            //{
+            //    int min = int.MaxValue;
+
+            //    foreach (int number in numbers)
+            //    {
+            //        if (number < min)
+            //        {
+            //            min = number;
+            //        }
+            //    }
+
+            //    return min;
+            //};
+            // Console.WriteLine(min(numbers));
+
+        }
         }
     }
-}
